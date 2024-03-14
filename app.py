ffa4030433a6f6de86ba32a7f7b46fdda23a485b
Tarikokc <tarik.oukaci@gmail.com>
2024-03-14 16:45:10 +0100

    Final

--- app.py
+++ app.py
@@ -1,32 +1,29 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
-from flask_socketio import join_room, leave_room
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-
-# app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'votre_cle_secrete'
 socketio = SocketIO(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('chatroom'))  # Rediriger vers la page du salon de discussion
     return render_template('home.html')
+
+@app.route('/chatroom')
+def chatroom():
+    if 'username' not in session:
+        return redirect(url_for('home'))
+    username = session['username']
+    return render_template('home.html', username=username)
 
 @socketio.on('message')
 def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
-    
-@socketio.on('join')
-def on_join(data):
-    room = data['room']
-    join_room(room)
-    send('Un utilisateur a rejoint le salon ' + room, room=room)
+    username = session.get('username', 'Utilisateur')
+    print(f'{username}: {msg}')
+    send(f'{username}: {msg}', broadcast=True)
 
-@socketio.on('leave')
-def on_leave(data):
-    room = data['room']
-    leave_room(room)
-    send('Un utilisateur a quitté le salon ' + room, room=room)
-    
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', debug=True)
